9e2d91f0795abb463852cb29c140b9e18e65d212
cristinatipabv <cristinatipa2022@gmail.com>
2026-03-17 19:45:31 +0200

    files

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("book_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {'form': form})
+
+def register_view(request):
+    # HTTP POST REQUEST
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            #user save in DB
+            form.save()
+            return redirect("login")
+    else:
+        # HTTP GET REQUEST
+        form = CustomUserCreationForm()
+    return render(request, "accounts/register.html", {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("book_list")
